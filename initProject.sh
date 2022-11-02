bf4f23ff7c17f4fcb440cbdb1c6539ad29e6b291
MichiK <48395928+Tokko55v2@users.noreply.github.com>
2022-11-02 20:49:01 +0100

    RSDK 01 fix path config

--- initProject.sh
+++ initProject.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 sh scripts/mint.sh
 
 swift run --package-path $MINT_PATH mint run yonaskolb/xcodegen@2.32.0 generate -s ./project.yml
--- scripts/mint.sh
+++ scripts/mint.sh
@@ -1,18 +1,16 @@
 #!bin/bash
 dir=$(pwd)
 echo $dir
-
 export MINT_PATH="$dir/.mint/Mint"
 
-if [ ! -d ".mint/Mint" ]; then
-  git clone https://github.com/yonaskolb/Mint.git .mint/Mint
+if [ ! -d "$MINT_PATH" ]; then
+  git clone https://github.com/yonaskolb/Mint.git $MINT_PATH
   swift run --package-path $MINT_PATH mint install yonaskolb/mint
 fi
 
 if [[ ! -z "$CI" ]]; then
-    echo "running on CI"
-    export PATH="$PATH:/Users/runner/.mint/bin"
-    export MINT_PATH="/Users/runner/.mint/Mint"
+  echo "running on CI"
+  export PATH="$PATH:/Users/runner/.mint/bin"
 fi
 
 swift run --package-path $MINT_PATH mint bootstrap
