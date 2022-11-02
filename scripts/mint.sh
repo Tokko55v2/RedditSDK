5924807386fa180b2e3d007e0f446d1fa114c88f
MichiK <48395928+Tokko55v2@users.noreply.github.com>
2022-11-02 19:23:17 +0100

    RSDK 01 export path on ci

--- scripts/mint.sh
+++ scripts/mint.sh
@@ -2,6 +2,11 @@
 dir=$(pwd)
 export MINT_PATH="$dir/.mint/Mint"
 
+if [[ ! -z "$CI" ]]; then
+    echo "running on CI"
+    export MINT_PATH="/Users/runner/.mint/bin"
+fi
+
 if [ ! -d ".mint/Mint" ]; then
   git clone https://github.com/yonaskolb/Mint.git .mint/Mint
   swift run --package-path $MINT_PATH mint install yonaskolb/mint
